6777e35ed19679839a811446b240b20d2b4fc575
chrisbann <christopher@bann.com>
2024-12-09 15:55:58 -0500

    our team

--- static/js/story.js
+++ static/js/story.js
@@ -1,20 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const storyContainer = document.getElementById("our-story");
-
-  // Fetch story from the JSON file
-  fetch("../static/json/story.json")
+    const storyContainer = document.getElementById("our-story");
+  
+    // Fetch team data from the JSON file
+    fetch("../static/json/story.json")
       .then(response => response.json())
-      .then(story => {
-          story.forEach(story => {
-              const card = document.createElement("div");
-              card.classList.add("story-card");
-
-              card.innerHTML = `
-                  <p>"${story.story}"</p>
-              `;
-
-              storyContainer.appendChild(card);
-          });
+      .then(teamMembers => {
+        teamMembers.forEach(member => {
+          const card = document.createElement("div");
+          card.classList.add("profile");
+  
+          card.innerHTML = `
+            <img src="${member.image}" alt="${member.name}">
+            <div class="overlay">
+              <p>${member.name}</p>
+              <p>${member.year}</p>
+              <p>${member.major}</p>
+            </div>
+          `;
+  
+          storyContainer.appendChild(card);
+        });
       })
-      .catch(error => console.error("Error loading story:", error));
-});
+      .catch(error => console.error("Error loading team data:", error));
+  });
+  
